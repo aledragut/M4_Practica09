0b52ce24756a3ba0f67266d2d841efb05e9263dd
aledragut <114613621+aledragut@users.noreply.github.com>
2023-02-01 20:08:19 +0100

    Add files via upload

---
+++ invest2.py
@@ -0,0 +1,13 @@
+# Aquest codi imprimeix el total dels diners acumulats després de 7 anys, utilitzant variables
+
+import math
+
+savings = 100
+
+increase = 1.1
+
+anys = 7
+
+result = savings * math.pow(increase, anys)
+
+print(result)
---
+++ sum.py
@@ -0,0 +1,3 @@
+# Este código realiza e imprime la suma entre 3 y 7
+
+print(3 + 7)
